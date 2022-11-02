dd8894f86eded6ef3f4989bb8add90e437a3df92
unknown <girdharshukla01@gmail.com>
2022-11-02 19:49:44 +0530

    Operators

---
+++ operators/Operators.java
@@ -0,0 +1,19 @@
+public class Operators{
+    public static void main(String[] args) {
+        // all same as C++
+
+        // new
+
+        int a = -1;
+        System.out.println(a>>1);
+        System.out.println(a>>>1);
+
+        Person obj = new Person();
+        System.out.println(obj instanceof Person);
+
+    }   
+}
+
+class Person{
+
+}
