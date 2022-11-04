d93a85234e524f128ba39d798f887e8731ab16b2
Logan <girdharshukla01@gmail.com>
2022-11-04 20:55:42 +0530

    Scanner

---
+++ input/ScannerDemo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;;
+public class ScannerDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Input Int : ");
+        int a = sc.nextInt();
+        System.out.println("Input world 'space' Sentence : ");
+        String b = sc.next();
+        String c = sc.nextLine();
+
+        System.out.println("Output : a = "+a+" b = "+b+" c = "+c);
+    }    
+}
