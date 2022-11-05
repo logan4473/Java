ad0212ec5b25f580a4b04ff5595f3ee707bd0042
Logan <girdharshukla01@gmail.com>
2022-11-05 22:28:21 +0530

    Printing

---
+++ printing/Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Prints with a line break");
+        System.out.print("Prints");
+        System.out.print("In");
+        System.out.print("Same");
+        System.out.print("Line");
+    }    
+}
