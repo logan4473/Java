e11ed7e53f484152d19e5b96ce252a1b9aff749d
Logan <girdharshukla01@gmail.com>
2022-11-13 19:05:06 +0530

    encapsulation

--- oops/Test.java
+++ oops/Test.java
@@ -1,6 +1,6 @@
 abstract class Shapes {
-    public String type;
-    public int dimentions;
+    protected String type;
+    protected int dimentions;
     public Shapes(String type,int dimentions){
         this.type = type;
         this.dimentions = dimentions;
@@ -11,7 +11,7 @@ abstract class Shapes {
 }
 
 class Circle extends Shapes {
-    public int radius;
+    protected int radius;
     public Circle(int radius){
         super("Circle", 2);
         this.radius = radius;
@@ -20,15 +20,27 @@ class Circle extends Shapes {
     @Override public double area(){
         return this.radius*this.radius*Math.PI;
     }
+
+    public int getRadius(){
+        return this.radius;
+    }
+
+    public String getType(){
+        return this.type;
+    }
+
+    public int getDimentions(){
+        return this.dimentions;
+    }
 }
 
 public class Test{
     public static void main(String[] args) {
         Circle c = new Circle(5);
 
-        System.out.println(c.type);
-        System.out.println(c.dimentions);
-        System.out.println(c.radius);
+        System.out.println(c.getType());
+        System.out.println(c.getDimentions());
+        System.out.println(c.getRadius());
         System.out.println(c.area());
     }
 }
