e31706a212ed14744ac62daa7e0f9a0039c9f5f3
Logan <girdharshukla01@gmail.com>
2022-11-27 13:22:43 +0530

    add at index

--- vectors/Vect.java
+++ vectors/Vect.java
@@ -3,18 +3,22 @@ import java.util.Vector;
 public class Vect {
     public static void main(String[] args) {
         Vector<Integer> v = new Vector<Integer>(10);
-        for(int i=1;i<=10;i++){
+        for (int i = 1; i <= 10; i++) {
             v.add(i);
         }
 
         System.out.println(v);
 
-        for(int i=0;i<v.size();i++){
-            System.out.println("v["+i+"] = "+v.get(i));
+        for (int i = 0; i < v.size(); i++) {
+            System.out.println("v[" + i + "] = " + v.get(i));
         }
 
         for (int i : v) {
             System.out.println(i);
         }
+
+        v.add(5, 45);
+        System.out.println(v);
+
     }
 }
