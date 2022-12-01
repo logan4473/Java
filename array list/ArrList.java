754dd66408800fa1f443592fce49fce88b7af684
Logan <girdharshukla01@gmail.com>
2022-12-01 22:43:44 +0530

    array list

---
+++ array list/ArrList.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class ArrList {
+    public static void main(String[] args) {
+        int n = 5;
+        ArrayList<Integer> arrli = new ArrayList<>(n);
+        for (int i = 1; i <= n; i++)
+            arrli.add(i);
+
+        System.out.println(arrli);
+
+        arrli.remove(3);
+        System.out.println(arrli);
+
+        for (int i = 0; i < arrli.size(); i++) {
+            System.out.print(arrli.get(i) + " ");
+        }
+        System.out.println();
+
+    }
+}
