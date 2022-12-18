81ace09cf16c6d6b6ab96cc8bb500c641ae78cfd
Logan <girdharshukla01@gmail.com>
2022-12-18 22:24:11 +0530

    hashmap

---
+++ hashmap/hashmap.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class hashmap {
+    public static void main(String[] args) {
+        HashMap<String, Integer> m = new HashMap<String, Integer>();
+
+        m.put("logan", 100);
+        m.put("captain", 101);
+        m.put("levis", 102);
+
+        System.out.println(m);
+    }
+}
